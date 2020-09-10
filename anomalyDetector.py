7659e2b411f6d09c8549e20acbe86a42e6f680c6
Alex Trinh <atrinh@dhcp-wifi-8021x-155-41-116-210.bu.edu>
2020-09-10 16:31:23 -0400

    added anomaly detector feature

---
+++ anomalyDetector.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+This example assumes the JSON data is saved one line per timestamp (message from server).
+
+It shows how to read and process a text file line-by-line in Python, converting JSON fragments
+to per-sensor dictionaries indexed by time.
+These dictionaries are immediately put into Pandas DataFrames for easier processing.
+
+Feel free to save your data in a better format--I was just showing what one might do quickly.
+"""
+import pandas
+from pathlib import Path
+import argparse
+import json
+from datetime import datetime
+import typing as T
+import matplotlib.pyplot as plt
+import numpy as np
+filer = open('output.txt')
+
+def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
+
+    temperature = {}
+    occupancy = {}
+    co2 = {}
+
+    with open(file, "r") as f:
+        for line in f:
+            r = json.loads(line)
+            room = list(r.keys())[0]
+            time = datetime.fromisoformat(r[room]["time"])
+
+            temperature[time] = {room: r[room]["temperature"][0]}
+            occupancy[time] = {room: r[room]["occupancy"][0]}
+            co2[time] = {room: r[room]["co2"][0]}
+
+        limit = 10   
+        counter = 0
+        runningAvg = 0
+        for k,v in temperature.items():
+            counter += 1
+            runningAvg = (list(v.values())[0] + runningAvg * (counter-1))/counter 
+            # An anomaly is detected if a value is too far from the average  
+            if list(v.values())[0] > runningAvg + limit or list(v.values())[0] < runningAvg - limit:
+                print("ANOMALY DETECTED!")
+
+    data = {
+        "temperature": pandas.DataFrame.from_dict(temperature, "index").sort_index(),
+        "occupancy": pandas.DataFrame.from_dict(occupancy, "index").sort_index(),
+        "co2": pandas.DataFrame.from_dict(co2, "index").sort_index(),
+    }
+
+    return data
+
+
+if __name__ == "__main__":
+    data = load_data('output.txt')
